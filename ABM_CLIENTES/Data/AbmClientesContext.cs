28b45786e20a13f528f2c093f2d403f6ae1a3945
mati <matiaslchamarez@gmail.com>
2024-06-03 14:26:06 -0300

    Agregar archivos de proyecto.

---
+++ ABM_CLIENTES/Data/AbmClientesContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using ABM_CLIENTES.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABM_CLIENTES.Data;
+
+public partial class AbmClientesContext : DbContext
+{
+    public AbmClientesContext()
+    {
+    }
+
+    public AbmClientesContext(DbContextOptions<AbmClientesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdCliente).HasName("PK__CLIENTES__23A341304DB051F9");
+
+            entity.ToTable("CLIENTES");
+
+            entity.Property(e => e.IdCliente).HasColumnName("ID_CLIENTE");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("APELLIDO");
+            entity.Property(e => e.Dni)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("DNI");
+            entity.Property(e => e.Domicilio)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("DOMICILIO");
+            entity.Property(e => e.FechaRegistro)
+                .HasColumnType("datetime")
+                .HasColumnName("FECHA_REGISTRO");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("NOMBRE");
+            entity.Property(e => e.NumCliente)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("NUM_CLIENTE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ABM_CLIENTES/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABM_CLIENTES.Models;
+
+public partial class Cliente
+{
+    public int IdCliente { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Dni { get; set; } = null!;
+    [Display(Name = "Número de Teléfono")]
+    public string NumCliente { get; set; } = null!;
+
+    public string Domicilio { get; set; } = null!;
+    [Display(Name = "Fecha de Registro")]
+    [DataType(DataType.Date)]
+    public DateTime? FechaRegistro { get; set; }
+}
